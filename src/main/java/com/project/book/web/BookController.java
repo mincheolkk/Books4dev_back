28a0c960977d2447413c585044358b533c6f03ce
mincheol <kmc04080408@gmail.com>
2021-11-27 01:47:59 +0900

    feat : 책 저장 컨트롤러

--- src/main/java/com/project/book/web/BookController.java
+++ src/main/java/com/project/book/web/BookController.java
@@ -1,14 +1,21 @@
 package com.project.book.web;
 
 
+import com.project.book.domain.Book;
+import com.project.book.dto.book.BookRequsetDto;
 import com.project.book.dto.kakao.KaKaoBookInfoDto;
 import com.project.book.service.BookService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,13 +24,11 @@ public class BookController {
     private final BookService bookService;
 
     // 책 저장 로직
-    @PostMapping()
-    public void saveBook(KaKaoBookInfoDto request) {
-        // isbn 으로 검색해서 디비에 없으면 추가
+    @PostMapping("/books")
+    public ResponseEntity saveBook(@RequestBody @Valid BookRequsetDto request) {
+        Book book = bookService.createOrRegisterBook(request);
+        URI uri = URI.create("/books/" + book.getId());
 
-        String isbn = request.getIsbn();
-        request.getAuthors();
-
-
+        return ResponseEntity.created(uri).build();
     }
 }
