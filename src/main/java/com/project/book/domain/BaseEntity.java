86e797cc678d926886f731697612619c3859c087
mincheol <kmc04080408@gmail.com>
2021-12-15 00:34:16 +0900

    refactor : BaseEntity 수정

--- src/main/java/com/project/book/domain/BaseEntity.java
+++ src/main/java/com/project/book/domain/BaseEntity.java
@@ -1,14 +1,13 @@
 package com.project.book.domain;
 
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
@@ -17,6 +16,7 @@ import java.time.LocalDateTime;
 public class BaseEntity {
 
     @CreatedDate
+    @Column(name = "createdAt", nullable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
@@ -24,4 +24,14 @@ public class BaseEntity {
 
     @CreatedBy
     private Long createdBy;
+
+//    @PrePersist
+//    public void prePersist() {
+//        createdAt = LocalDateTime.now();
+//    }
+//
+//    @PreUpdate
+//    public void preUpdate() {
+//        modifiedAt = LocalDateTime.now();
+//    }
 }
