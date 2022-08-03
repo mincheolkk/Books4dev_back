6b98e8fd4f08bbaaed3c93ac1d7ac63633266633
mincheolkk <kmc04080408@gmail.com>
2022-08-03 15:03:27 +0900

    feat : 등록한 관심있는 책들 뿌려주는 기능

--- src/main/java/com/project/book/book/repository/WishMemberRepositoryCustom.java
+++ src/main/java/com/project/book/book/repository/WishMemberRepositoryCustom.java
@@ -1,10 +1,16 @@
 package com.project.book.book.repository;
 
 import com.project.book.book.domain.WishBook;
+import com.project.book.book.domain.WishMember;
+import com.project.book.book.dto.response.WishBookResponseDto;
 import com.project.book.member.domain.Member;
+
+import java.util.List;
 
 public interface WishMemberRepositoryCustom {
 
     boolean findByWishBook(WishBook wishBook, Member member);
+
+    List<WishBookResponseDto> getAllWishBook(Member member);
 
 }
--- src/main/java/com/project/book/book/repository/service/WishMemberRepositoryImpl.java
+++ src/main/java/com/project/book/book/repository/service/WishMemberRepositoryImpl.java
@@ -1,14 +1,22 @@
 package com.project.book.book.repository.service;
 
+import com.project.book.book.domain.QWishBook;
 import com.project.book.book.domain.QWishMember;
 import com.project.book.book.domain.WishBook;
 import com.project.book.book.domain.WishMember;
+import com.project.book.book.dto.response.QWishBookResponseDto;
+import com.project.book.book.dto.response.WishBookResponseDto;
 import com.project.book.book.repository.WishMemberRepositoryCustom;
 import com.project.book.member.domain.Member;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.project.book.book.domain.QWishBook.*;
+import static com.project.book.book.domain.QWishMember.*;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,11 +27,24 @@ public class WishMemberRepositoryImpl implements WishMemberRepositoryCustom {
 
 
     public boolean findByWishBook(WishBook wishBook, Member member){
-        return queryFactory.selectFrom(QWishMember.wishMember)
+        return queryFactory.selectFrom(wishMember)
                 .where(
-                        QWishMember.wishMember.member.eq(member),
-                        QWishMember.wishMember.wishBook.eq(wishBook)
+                        wishMember.member.eq(member),
+                        wishMember.wishBook.eq(wishBook)
                 ).fetchOne() != null;
+    }
 
+    public List<WishBookResponseDto> getAllWishBook(Member member) {
+        List<WishBookResponseDto> fetch = queryFactory.select(new QWishBookResponseDto(
+                        wishBook.title, wishBook.isbn, wishBook.thumbnail
+                ))
+                .from(wishMember)
+                .join(wishMember.wishBook, wishBook)
+                .where(wishMember.member.eq(member))
+                .fetch();
+
+
+        System.out.println("fetch = " + fetch);
+        return fetch;
     }
 }
