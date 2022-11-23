6bfe74d6ae20c0baba0ce17695543f37bb5deb4e
mincheolkk <kmc04080408@gmail.com>
2022-11-23 18:20:43 +0900

    feat : health 체크용 컨트롤러

--- src/main/java/com/project/book/common/controller/Healthcontroller.java
+++ src/main/java/com/project/book/common/controller/Healthcontroller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Healthcontroller {
 
-    @GetMapping
+    @GetMapping(value = "/health")
     public String healthCheck() {
         return "health ok";
     }
