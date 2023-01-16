46045b38d8b9b763ac5fb177732b126ba1f45c77
mincheolkk <kmc04080408@gmail.com>
2023-01-16 23:10:31 +0900

    refactor : Book 엔티티 반환하는 메서드 생성

--- src/main/java/com/project/book/book/dto/request/RegisterBySearchDto.java
+++ src/main/java/com/project/book/book/dto/request/RegisterBySearchDto.java
@@ -1,8 +1,15 @@
 package com.project.book.book.dto.request;
 
+import com.project.book.book.domain.Book;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
+
+import java.util.List;
+import java.util.Objects;
+
+import static com.project.book.common.utils.ServiceUtils.listToString;
 
 @Getter
 @NoArgsConstructor
@@ -11,9 +18,17 @@ public class RegisterBySearchDto {
     private BookDataDto item;
     private BookReviewDto review;
 
-    @Builder
-    public RegisterBySearchDto(BookDataDto item, BookReviewDto review) {
-        this.item = item;
-        this.review = review;
+    public Book toBook(BookDataDto request) {
+        return Book.builder()
+                .authors(listToString(request.getAuthors()))
+                .translators(listToString(request.getTranslators()))
+                .title(request.getTitle())
+                .publisher(request.getPublisher())
+                .price(request.getPrice())
+                .thumbnail(request.getThumbnail())
+                .releaseDate(request.getDatetime().toLocalDateTime())
+                .isbn(request.getIsbn())
+                .contents(request.getContents())
+                .build();
     }
 }
