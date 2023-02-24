bffd1a03b861c3a4f7e86f299f782d4261d2119a
mincheolkk <kmc04080408@gmail.com>
2023-02-25 00:53:41 +0900

    feat : 엔티티 생성 메서드 및 유효성 체크 메서드 추가

--- src/main/java/com/project/book/book/dto/request/BookDataDto.java
+++ src/main/java/com/project/book/book/dto/request/BookDataDto.java
@@ -1,5 +1,7 @@
 package com.project.book.book.dto.request;
 
+import com.project.book.book.domain.Book;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,7 +9,11 @@ import lombok.NoArgsConstructor;
 import java.time.ZonedDateTime;
 import java.util.List;
 
+import static com.project.book.common.utils.ServiceUtils.listToString;
+
+@Builder
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
 public class BookDataDto {
 
@@ -21,4 +27,24 @@ public class BookDataDto {
     private String thumbnail;
     private String contents;
 
+    public Book toBook() {
+        return Book.builder()
+                .authors(listToString(this.authors))
+                .translators(listToString(this.translators))
+                .title(this.title)
+                .publisher(this.publisher)
+                .price(this.price)
+                .thumbnail(this.thumbnail)
+                .releaseDate(this.datetime != null ? this.datetime.toLocalDateTime() : null)
+                .isbn(this.isbn)
+                .contents(this.contents)
+                .build();
+    }
+
+    public Boolean validCheck() {
+        if (this.isbn.length() < 1) {
+            return false;
+        }
+        return true;
+    }
 }
