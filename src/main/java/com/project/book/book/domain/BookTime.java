5e2b007ad5ae8c866bb979af84cfd975201112af
mincheolkk <kmc04080408@gmail.com>
2023-02-27 21:42:38 +0900

    feat : enum 안에서 메서드 추가

--- src/main/java/com/project/book/book/domain/BookTime.java
+++ src/main/java/com/project/book/book/domain/BookTime.java
@@ -1,13 +1,36 @@
 package com.project.book.book.domain;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
+import com.querydsl.core.types.OrderSpecifier;
+
+import java.util.Arrays;
+
+import static com.project.book.book.domain.QBook.book;
 
 public enum BookTime {
 
-    before,
-    after,
-    twoYear,
-    fiveYear,
-    tenYear,
-    All;
+    before("before",book.recommendTime.beforeCount.desc()),
+    after("after",  book.recommendTime.afterCount.desc()),
+    twoYear("twoYear", book.recommendTime.twoYearCount.desc()),
+    fiveYear("fiveYear", book.recommendTime.fiveYearCount.desc()),
+    tenYear("tenYear",  book.recommendTime.tenYearCount.desc()),
+    All("All", book.star.avgStar.desc());
+
+    private final String name;
+    private final OrderSpecifier<?> orderSpecifier;
+
+    BookTime(String name, OrderSpecifier<?> orderSpecifier) {
+        this.name = name;
+        this.orderSpecifier = orderSpecifier;
+    }
+
+    public static BookTime fromBookTime(BookTime condition) {
+        return Arrays.stream(BookTime.values())
+                .filter(bookTime -> bookTime.name.equals(condition != null ? condition.name : All))
+                .findAny()
+                .orElse(All);
+    }
+
+    public OrderSpecifier<?> getOrderSpecifier() {
+        return orderSpecifier;
+    }
 }
