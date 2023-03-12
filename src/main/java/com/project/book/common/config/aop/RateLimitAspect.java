d567d5462b40e3814c6f809e6eac0acba4df6d3a
mincheolkk <kmc04080408@gmail.com>
2023-03-13 01:17:40 +0900

    feat : client ip 파악하는 메서드 추가

--- src/main/java/com/project/book/common/config/aop/RateLimitAspect.java
+++ src/main/java/com/project/book/common/config/aop/RateLimitAspect.java
@@ -10,6 +10,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -27,12 +28,33 @@ public class RateLimitAspect {
     public Object rateLimitAround(ProceedingJoinPoint joinPoint) throws Throwable {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
-        String identifier = request.getRemoteAddr();
+        String identifier = getClientIpAddr(request);
         String methodName = joinPoint.getSignature().getName();
 
-        if (!rateLimiter.isOneMethodAllowed(identifier, methodName, 1, 5) || !rateLimiter.isAllMethodAllowed(identifier,5,20)) {
+        if (!rateLimiter.isAllMethodAllowed(identifier,5,50)) {
             throw new TooManyRequestException();
         }
         return joinPoint.proceed();
+    }
+
+    public static String getClientIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
     }
 }
