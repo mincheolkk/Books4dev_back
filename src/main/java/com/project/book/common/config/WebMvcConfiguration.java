30590a793a3986f75fff11756d62f20523d3c0f9
mincheolkk <kmc04080408@gmail.com>
2023-03-28 03:12:02 +0900

    feat : interceptor에서 oauth넣어주는 부분을 argumentresolver로 변경

--- src/main/java/com/project/book/common/config/WebMvcConfiguration.java
+++ src/main/java/com/project/book/common/config/WebMvcConfiguration.java
@@ -1,14 +1,11 @@
 package com.project.book.common.config;
 
-import com.project.book.common.config.jwt.BearerAuthInterceptor;
 import com.project.book.common.config.jwt.MemberArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -16,18 +13,10 @@ import java.util.List;
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
     private final MemberArgumentResolver MemberArgumentResolver;
-    private final BearerAuthInterceptor bearerAuthInterceptor;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(MemberArgumentResolver);
     }
 
-    @Override
-    public void addInterceptors(final InterceptorRegistry registry) {
-        final List<String> pathPatterns = List.of(
-                "/auth/out","/auth/refreshtoken"
-        );
-        registry.addInterceptor(bearerAuthInterceptor).addPathPatterns(pathPatterns);
-    }
 }
