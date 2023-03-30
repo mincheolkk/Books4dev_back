240cfacaaf0c1615525cf60a8199662f01c9cabf
mincheolkk <kmc04080408@gmail.com>
2023-03-31 02:48:40 +0900

    feat : validate 추가

--- src/main/java/com/project/book/book/domain/Comment.java
+++ src/main/java/com/project/book/book/domain/Comment.java
@@ -1,40 +1,47 @@
 package com.project.book.book.domain;
 
 import com.project.book.common.domain.BaseEntity;
+import com.project.book.common.exception.InvalidLengthException;
 import com.project.book.member.domain.Member;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
+@DynamicUpdate
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment extends BaseEntity {
 
-    public static final int MIN_CONTENT_LENGTH = 1;
     public static final int MAX_CONTENT_LENGTH = 200;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     @Column(length = MAX_CONTENT_LENGTH)
     private String content;
 
+    @NotNull
     private Long bookId;
+
+    @NotNull
     private Long memberId;
 
+    @NotNull
     @Column(name = "comment_oAuth")
     private String oAuth;
 
+    @NotNull
     @Column(name = "comment_nickname")
     private String nickname;
 
     @Builder
     public Comment(Long id, String content, Long bookId, Long memberId, String oAuth, String nickname) {
+        validate(content);
         this.id = id;
         this.content = content;
         this.bookId = bookId;
@@ -48,6 +55,14 @@ public class Comment extends BaseEntity {
     }
 
     public void updateContent(String content) {
+        validate(content);
         this.content = content;
     }
+
+    public void validate(String content) {
+        if (content.isBlank() || content.length() > MAX_CONTENT_LENGTH) {
+            throw new InvalidLengthException();
+        }
+    }
+
 }
