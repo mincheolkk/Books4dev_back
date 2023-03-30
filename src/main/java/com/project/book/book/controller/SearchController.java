3fe00d4ac4ed609e805ef401d668ce836eaad546
mincheolkk <kmc04080408@gmail.com>
2023-03-31 02:49:25 +0900

    refactor : 이름변경 및 검색 컨트롤러 모음

--- src/main/java/com/project/book/book/controller/SearchController.java
+++ src/main/java/com/project/book/book/controller/SearchController.java
@@ -1,25 +1,27 @@
-package com.project.book.member.controller;
+package com.project.book.book.controller;
 
 import com.project.book.book.dto.kakao.KakaoBookDto;
+import com.project.book.book.service.BookService;
 import com.project.book.common.utils.RedisUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 
 @RestController
-@RequestMapping("/kakao")
 @RequiredArgsConstructor
-public class KakaoController {
+public class SearchController {
 
     private final WebClient kakaoWebClient;
     private final RedisUtil redisUtil;
+    private final BookService bookService;
 
-    @GetMapping("/search")
+    // 책 검색 - 카카오 책검색 api
+    @GetMapping("/kakao/search")
     public Mono<KakaoBookDto> search(@RequestParam final String query) {
-        System.out.println("query = " + query);
-
         Mono<KakaoBookDto> kakaoBookDtoMono = kakaoWebClient.get()
                 .uri(builder -> builder.path("/v3/search/book")
                         .queryParam("page", "1")
@@ -38,6 +40,12 @@ public class KakaoController {
         return kakaoBookDtoMono;
     }
 
-
-
+    // 책 검색 - books4dev 데이터 검색
+    @GetMapping("/book/search/readbook")
+    public ResponseEntity<?> findBookBySearch(@RequestParam final String query) {
+        if (query.length() < 2) {
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(bookService.findBookBySearch(query), HttpStatus.OK);
+    }
 }
