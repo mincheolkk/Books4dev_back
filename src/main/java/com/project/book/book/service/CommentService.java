11506974dcb5809a0d3b89eb2baa9417de266107
mincheolkk <kmc04080408@gmail.com>
2023-04-03 17:51:31 +0900

    refactor : @LoginMember 반환 타입 수정에 따른 서비스 로직 변경

--- src/main/java/com/project/book/book/service/CommentService.java
+++ src/main/java/com/project/book/book/service/CommentService.java
@@ -9,6 +9,7 @@ import com.project.book.book.repository.CommentRepository;
 import com.project.book.common.exception.ContentNotFoundException;
 import com.project.book.common.exception.InvalidOwnerException;
 import com.project.book.member.domain.Member;
+import com.project.book.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,12 +20,14 @@ import java.util.List;
 
 import static com.project.book.common.domain.EntityStatus.*;
 
-@Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
 public class CommentService {
 
     private final CommentRepository commentRepository;
     private final BookRepository bookRepository;
+    private final MemberRepository memberRepository;
 
     public ResponseEntity<?> getComments(final Long bookId) {
         List<CommentResponseDto> commentList = commentRepository.findCommentListByBook(bookId);
@@ -33,10 +36,11 @@ public class CommentService {
     }
 
     @Transactional
-    public Long saveComment(final Member member, final Long bookId, final SaveCommentRequestDto request) {
+    public Long saveComment(final String oAuth, final Long bookId, final SaveCommentRequestDto request) {
+        Member member = memberRepository.findByoAuth(oAuth);
+
         Comment comment = request.toComment(member.getId(), bookId, member.getNickname().getNickname(), member.getOAuth());
         commentRepository.save(comment);
-
         Book book = bookRepository.findById(bookId).orElseThrow(
                 () -> new ContentNotFoundException());
 
@@ -45,7 +49,9 @@ public class CommentService {
     }
 
     @Transactional
-    public ResponseEntity<?> deleteComment(final Member member, final Long commentId) {
+    public ResponseEntity<?> deleteComment(final String oAuth, final Long commentId) {
+        Member member = memberRepository.findByoAuth(oAuth);
+
         Comment comment = validateOwner(member, commentId);
         comment.changeDeleteYn(DeleteYn.Y);
 
@@ -58,10 +64,11 @@ public class CommentService {
     }
 
     @Transactional
-    public ResponseEntity<?> updateComment(final Member member, final Long commentId,
+    public ResponseEntity<?> updateComment(final String oAuth, final Long commentId,
                                            final SaveCommentRequestDto request) {
-        Comment comment = validateOwner(member, commentId);
+        Member member = memberRepository.findByoAuth(oAuth);
 
+        Comment comment = validateOwner(member, commentId);
         comment.updateContent(request.getContent());
 
         return new ResponseEntity(HttpStatus.OK);
