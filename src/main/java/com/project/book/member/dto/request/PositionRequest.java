883948966837979c9a2e281fec5af2612f7e783f
mincheolkk <kmc04080408@gmail.com>
2023-04-03 17:46:10 +0900

    style : 위치 및 이름 변경

--- src/main/java/com/project/book/member/dto/request/PositionRequest.java
+++ src/main/java/com/project/book/member/dto/request/PositionRequest.java
@@ -1,4 +1,4 @@
-package com.project.book.member.dto.response;
+package com.project.book.member.dto.request;
 
 import com.project.book.member.domain.MemberType;
 import lombok.Getter;
@@ -9,7 +9,7 @@ import javax.persistence.Enumerated;
 
 @NoArgsConstructor
 @Getter
-public class PositionResponseDto {
+public class PositionRequest {
 
     @Enumerated(value = EnumType.STRING)
     private MemberType position;
